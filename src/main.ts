350053c6d9b6c215816be6c807eb08b0391287c4
unknown <hungk63bka@gmail.com>
2022-07-21 06:08:10 +0700

    fix deploy

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  const PORT = process.env.PORT || 5000;
+  const PORT = parseInt(`${process.env.PORT}`) || 5000;
   await app.listen(PORT);
 }
 bootstrap();
